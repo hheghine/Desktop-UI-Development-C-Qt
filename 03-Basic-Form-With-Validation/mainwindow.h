6a5761a66b2e673f6ce21f395d0b318f3215c517
heghine <heghine.balasanyan@gmail.com>
2024-02-24 04:58:25 +0400

    task03: start

---
+++ 03-Basic-Form-With-Validation/mainwindow.h
@@ -0,0 +1,30 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QLineEdit>
+#include <QPushButton>
+#include <QFormLayout>
+#include <QMessageBox>
+#include <QLabel>
+#include <QGridLayout>
+
+QT_BEGIN_NAMESPACE
+namespace Ui {
+class MainWindow;
+}
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private:
+    Ui::MainWindow *ui;
+};
+
+#endif // MAINWINDOW_H
